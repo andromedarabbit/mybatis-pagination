d00efeb1c4812c6fefe0a8ceb164acd1cb087ae5
Jaehoon Choi <plaintext@andromedarabbit.net>
2016-05-25 14:11:13 +0900

    Annotation-based configuration instead of XML-based one
    - MariaDB connector instead of MySQL one

--- src/test/java/net/andromedarabbit/mybatis/pagination/SelectLimitByTest.java
+++ src/test/java/net/andromedarabbit/mybatis/pagination/SelectLimitByTest.java
@@ -3,27 +3,30 @@ package net.andromedarabbit.mybatis.pagination;
 /**
  * Created by tywin on 2015. 11. 16..
  */
+
+import net.andromedarabbit.configuration.MyBatisConfig;
+import net.andromedarabbit.configuration.TestConfiguration;
 import net.andromedarabbit.persistence.dao.MysqlUserDao;
 import net.andromedarabbit.persistence.model.MysqlUser;
 import net.andromedarabbit.persistence.model.MysqlUserExample;
 import org.apache.ibatis.session.RowBounds;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.inject.Inject;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({
-        "classpath:application-context.xml"
+@ContextConfiguration(classes = {
+        TestConfiguration.class, MyBatisConfig.class
 })
 public class SelectLimitByTest {
-    @Autowired
+    @Inject
     private MysqlUserDao dao;
 
     @Test
